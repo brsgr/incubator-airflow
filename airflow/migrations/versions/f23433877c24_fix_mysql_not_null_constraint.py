58481cca4c26360a9156eb615c7578b2c28125c1
Robert Grado <rgrado@rg-c1mtl35aj1wl.local>
2018-12-20 11:48:59 -0500

    mysql changes to bypass explicit_defaults exceptioon

--- airflow/migrations/versions/f23433877c24_fix_mysql_not_null_constraint.py
+++ airflow/migrations/versions/f23433877c24_fix_mysql_not_null_constraint.py
@@ -26,6 +26,7 @@ Create Date: 2018-06-17 10:16:31.412131
 """
 from alembic import op
 from sqlalchemy.dialects import mysql
+import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = 'f23433877c24'
@@ -38,15 +39,15 @@ def upgrade():
     conn = op.get_bind()
     if conn.dialect.name == 'mysql':
         conn.execute("SET time_zone = '+00:00'")
-        op.alter_column('task_fail', 'execution_date', existing_type=mysql.TIMESTAMP(fsp=6), nullable=False)
-        op.alter_column('xcom', 'execution_date', existing_type=mysql.TIMESTAMP(fsp=6), nullable=False)
-        op.alter_column('xcom', 'timestamp', existing_type=mysql.TIMESTAMP(fsp=6), nullable=False)
+        op.alter_column('task_fail', 'execution_date', existing_type=mysql.TIMESTAMP(fsp=6), nullable=False, server_default=sa.text('CURRENT_TIMESTAMP(6)'))
+        op.alter_column('xcom', 'execution_date', existing_type=mysql.TIMESTAMP(fsp=6), nullable=False, server_default=sa.text('CURRENT_TIMESTAMP(6)'))
+        op.alter_column('xcom', 'timestamp', existing_type=mysql.TIMESTAMP(fsp=6), nullable=False, server_default=sa.text('CURRENT_TIMESTAMP(6)'))
 
 
 def downgrade():
     conn = op.get_bind()
     if conn.dialect.name == 'mysql':
         conn.execute("SET time_zone = '+00:00'")
-        op.alter_column('xcom', 'timestamp', existing_type=mysql.TIMESTAMP(fsp=6), nullable=True)
-        op.alter_column('xcom', 'execution_date', existing_type=mysql.TIMESTAMP(fsp=6), nullable=True)
-        op.alter_column('task_fail', 'execution_date', existing_type=mysql.TIMESTAMP(fsp=6), nullable=True)
+        op.alter_column('xcom', 'timestamp', existing_type=mysql.TIMESTAMP(fsp=6), nullable=False, server_default=sa.text('CURRENT_TIMESTAMP(6)'))
+        op.alter_column('xcom', 'execution_date', existing_type=mysql.TIMESTAMP(fsp=6), nullable=False, server_default=sa.text('CURRENT_TIMESTAMP(6)'))
+        op.alter_column('task_fail', 'execution_date', existing_type=mysql.TIMESTAMP(fsp=6), nullable=False, server_default=sa.text('CURRENT_TIMESTAMP(6)'))
